6fc4458e69be8ddc8a649eaf6720fe6450646f0f
Hamza Khan <58217684+hamxa678@users.noreply.github.com>
2022-09-21 00:17:04 +0500

    Update Hello World!.asm

--- L1/Hello World!.asm
+++ L1/Hello World!.asm
@@ -1,6 +1,8 @@
 .data
-    myMessage: .asciiz "Hello World! \n"
+    myMessage: .asciiz "Hello World! \n"  # Defining a string in RAM
+    
 .text
+    # Printing a string on the screen
     li $v0, 4
     la $a0, myMessage
     syscall
