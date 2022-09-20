7bf0b586bfde7d3ba625db1e1de461f2cfeadf9f
Hamza Khan <58217684+hamxa678@users.noreply.github.com>
2022-09-21 00:20:12 +0500

    Printing a Character
    Assembly Language code for printing character.

--- L2/Printing a Character.asm
+++ L2/Printing a Character.asm
@@ -1,6 +1,8 @@
 .data
-    myCharacter: .byte 'H'
+    myCharacter: .byte 'H'  # Defining char in RAM having a value 'H'
+    
 .text
+    # Printing a char value on the screen
     li $v0, 4
     la $a0, myCharacter
     syscall
