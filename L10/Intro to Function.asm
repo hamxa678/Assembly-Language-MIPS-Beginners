ce39434a23e80095b7b5c0216a11beffb7528c89
Hamza <58217684+hamxa678@users.noreply.github.com>
2022-09-23 23:54:09 +0500

    Intro to Function
    Assembly Language code for Function printing String message.

--- L10/Intro to Function.asm
+++ L10/Intro to Function.asm
@@ -4,16 +4,20 @@
 .text
     main:
     	jal displayMessage                    # Transfering Control to line number where the funtion is define .i.e. here line no will be 12
-    	
+    
+    
     #Telling the assembler that the program is done.
     li $v0, 10
     syscall
     
-    displayMessage:                           
+    displayMessage:                           # Function Defination
+    
+        #printing message
     	li $v0, 4
     	la $a0, message
     	syscall
     	
+        
     	jr $ra
         Intro to Function
         Assembly Language code for Function printing String message.
