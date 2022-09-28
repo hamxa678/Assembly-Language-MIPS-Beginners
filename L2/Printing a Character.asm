05df905e6a6537be5a0231b3b7b218b11e260302
Hamza <58217684+hamxa678@users.noreply.github.com>
2022-09-28 23:55:20 +0500

    Printing a Character
    Assembly Language code for printing a Character

--- L2/Printing a Character.asm
+++ L2/Printing a Character.asm
@@ -6,5 +6,3 @@
     li $v0, 4                   #Storing instant value 4 in $v0 means that computer is going to print a string/char.
     la $a0, myCharacter         #Storing value of myCharacter in $a0, means that the string/char value computer is about to print is myCharacter value
     syscall                     #System call tell the assembler to perform the above required task. i.e. printing a String/char
-    Printing a Character
-    Assembly Language code for printing a Character
