555760a501b10321a0ea7ec5d5beeccec94ad400
Hamza <58217684+hamxa678@users.noreply.github.com>
2022-09-29 23:54:17 +0500

    Printing an Integer
    Assembly Language code for printing an integer

--- L3/Printing an Integer.asm
+++ L3/Printing an Integer.asm
@@ -3,7 +3,7 @@
     
 .text
     # Printing an integer on the screen
-    li $v0, 1               #Storing instant value 1 in $v0 means that computer is going to print a string/char.   
+    li $v0, 1               #Storing instant value 1 in $v0 means that computer is going to print a integer.   
     lw $a0, myInt
     syscall
     
