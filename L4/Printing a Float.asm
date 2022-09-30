9980647632bd448c9da3a06bb6d6517b50d8b5c2
Hamza <58217684+hamxa678@users.noreply.github.com>
2022-09-30 23:55:19 +0500

    Printing a Float
    Assembly Language code for printing a Float.

--- L4/Printing a Float.asm
+++ L4/Printing a Float.asm
@@ -6,6 +6,3 @@
     li $v0, 2               #Storing instant value 2 in $v0 means that computer is going to print a float.
     lwc1 $f12, myFloat      #Storing value of myFloat in $f12 of co processor, means that the float value that computer is about to print is myFloat value
     syscall                 #System call tell the assembler to perform the above required task. i.e. printing an float
-    
-    
-    
