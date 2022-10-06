c6b68c2f1e0fcc8350635c4beb6dba30e4486bb8
Hamza <58217684+hamxa678@users.noreply.github.com>
2022-10-06 23:56:06 +0500

    Printing a Double
    Assembly Language code for Printing a Double.

--- L5/Printing a Double.asm
+++ L5/Printing a Double.asm
@@ -4,7 +4,7 @@
     
 .text
     # Printing a double value on the screen
-    ldc1 $f2, myDouble          
+    ldc1 $f2, myDouble          #Storing value of myDouble in $f2 of co processor.     
     ldc1 $f0, zeroDouble
     
     li $v0, 3                   #Storing instant value 3 in $v0 means that computer is going to print a double.
