84e7dfae6c6cb5e39f6ac90cc46f632e6737361a
Hamza <58217684+hamxa678@users.noreply.github.com>
2022-10-06 23:54:40 +0500

    Printing a Double
    Assembly Language code for Printing a Double

--- L5/Printing a Double.asm
+++ L5/Printing a Double.asm
@@ -9,7 +9,7 @@
     
     li $v0, 3                   #Storing instant value 3 in $v0 means that computer is going to print a double.
     add.d $f12,$f2, $f0         #adding a decimal numbers stored in $f2 and $f0 using add.d, and storing the resultant value in $f12 mean we are telling the computer that the double value you were about to print is in $f12.
-    syscall
+    syscall                     #System call tell the assembler to perform the above required task. i.e. printing an double
     
     Printing a Double
     Assembly Language code for Printing a Double.
