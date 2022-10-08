71bc76867f9f34697578836441591ad03b7da1ec
Hamza <58217684+hamxa678@users.noreply.github.com>
2022-10-08 23:55:59 +0500

    Subtracting Integers
    Assembly Language Code for Subtracting Integers

--- L7/Subtracting Integers.asm
+++ L7/Subtracting Integers.asm
@@ -13,6 +13,3 @@
     li $v0, 1               # As explained in previous coding
     add $a0, $zero, $t2		# you can also use "move $a0, $t2" instead of "add $a0, $zero, $t2"
     syscall
-    
-    Subtracting Integers
-    Assembly Language Code for Subtracting Integers
